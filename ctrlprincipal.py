084af8da20d8f427b598ad9f8f0c60e9e72c0f36
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2023-10-02 18:40:24 -0300

    Desenvolvimento da Tela Principal e Controlador.

---
+++ ctrlprincipal.py
@@ -0,0 +1,28 @@
+from telaprincipal import TelaPrincipal
+
+
+class CtrlPrincipal:
+    def __init__(self):
+        pass
+
+    def mostra_tela_principal(self):
+        tela = TelaPrincipal()
+        op = tela.opcoes_do_sistema
+        self.processa_input(op)
+
+    def mostra_tela_movimentos(self):
+        pass
+
+    def mostra_tela_cadastros(self):
+        pass
+
+    def mostra_tela_financeiro(self):
+        pass
+
+    def processa_input(self,op):
+        if op == 1:
+            self.mostra_tela_movimentos
+        elif op == 2:
+            self.mostra_tela_cadastros
+        elif op == 3:
+            self.mostra_tela_financeiro
