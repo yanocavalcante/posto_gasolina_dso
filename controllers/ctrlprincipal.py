1461a7c80ac16072952a928ed027f4d445624854
Yano Cavalcante <yano.melo.cavalcante@gmail.com>
2023-10-02 21:12:13 -0300

    Definição de Pastas

--- controllers/ctrlprincipal.py
+++ controllers/ctrlprincipal.py
@@ -7,7 +7,7 @@ class CtrlPrincipal:
 
     def mostra_tela_principal(self):
         tela = TelaPrincipal()
-        op = tela.opcoes_do_sistema
+        op = tela.opcoes_do_sistema()
         self.processa_input(op)
 
     def mostra_tela_movimentos(self):
@@ -21,8 +21,8 @@ class CtrlPrincipal:
 
     def processa_input(self,op):
         if op == 1:
-            self.mostra_tela_movimentos
+            self.mostra_tela_movimentos()
         elif op == 2:
-            self.mostra_tela_cadastros
+            self.mostra_tela_cadastros()
         elif op == 3:
-            self.mostra_tela_financeiro
+            self.mostra_tela_financeiro()
