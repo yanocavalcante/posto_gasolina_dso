9286c7d3f07a610dca6a1790fca936a59187505b
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-10-06 20:42:32 -0300

    Getters da classe Nota

--- models/nota.py
+++ models/nota.py
@@ -5,8 +5,17 @@ class Nota(ABC):
     @abstractmethod
     def __init__(self):
         self.__produtos = []
+        self.__caixa = caixa
+        self.__valor = valor
     
     @property
     def produtos(self):
         return self.__produtos
     
+    @property
+    def caixa(self):
+        return self.__caixa
+    
+    @property
+    def valor(self):
+        return self.__valor
