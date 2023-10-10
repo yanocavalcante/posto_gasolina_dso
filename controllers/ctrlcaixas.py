1ee6ebbf401e50fde65eb9016ad9d2f7fb73ba58
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-10-09 21:24:54 -0300

    Melhorias na Aba 'Caixas'

--- controllers/ctrlcaixas.py
+++ controllers/ctrlcaixas.py
@@ -1,15 +1,23 @@
+from views.telacaixa import TelaCaixa
+from models.caixa import Caixa
+
+
 class CtrlCaixas:
     def __init__(self, ctrlprincipal):
         self.__ctrlprincipal = ctrlprincipal
-    
+        self.__telacaixa = TelaCaixa()
+        self.__listacaixas = []
+        
     def pergunta_caixa(self):
-        pass
+        caixa = self.__telacaixa.seleciona_caixa
+        for caixas in self.__listacaixas:
+            if caixa == caixas.nome:
+                pass
 
     def pergunta_acao(self):
-        pass
-
-    def cria_caixa(self):
-        pass
-
-    def altera_caixa(self):
-        pass
+        op = self.__telacaixa.seleciona_acao
+        if op == 1:
+            self.cria_caixa
+        
+        elif op == 2:
+            self.pergunta_caixa
--- models/caixa.py
+++ models/caixa.py
@@ -1,17 +1,12 @@
 class Caixa:
-    def __init__(self, saldo, banco, credito):
+    def __init__(self, saldo, tipo):
         self.__saldo = saldo
-        self.__banco = banco
-        self.__credito = credito
+        self.__tipo = tipo
 
     @property
     def saldo(self):
         return self.__saldo
     
     @property
-    def banco(self):
-        return self.__banco
-    
-    @property
-    def credito(self):
-        return self.__credito
+    def tipo(self):
+        return self.__tipo
