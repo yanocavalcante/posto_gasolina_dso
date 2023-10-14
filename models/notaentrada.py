a7a87c0ed4bc815ee796b23f622133581d2caad8
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-10-14 17:05:28 -0300

    Melhorias

--- models/notaentrada.py
+++ models/notaentrada.py
@@ -2,7 +2,7 @@ from models.nota import Nota
 
 
 class NotaEntrada(Nota):
-    def __init__(self,produtos: list, fornecedor: str):
-        super().__init__(produtos)
+    def __init__(self,produtos: list, num: str, caixa, valor: float, fornecedor: str):
+        super().__init__(produtos, num, caixa, valor)
         self.__fornecedor = fornecedor
         
