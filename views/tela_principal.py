bd96e01230c9e1ae749895c050c7840b13d3447c
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-10-14 22:28:16 -0300

    Problema com o input de ações

--- views/tela_principal.py
+++ views/tela_principal.py
@@ -2,7 +2,7 @@ class TelaPrincipal:
 
     def opcoes_do_sistema(self):
         print('1 - Notas       2 - Cadastros      3 - Caixas       4 - Sair')
-        op = input()
+        op = int(input())
         
         return op
     
