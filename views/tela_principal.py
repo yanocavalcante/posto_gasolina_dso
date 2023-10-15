73a26a9daa4c886304ec9a18b0dfd4680e29b215
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-10-14 23:03:45 -0300

    Funções de Estilização

--- views/tela_principal.py
+++ views/tela_principal.py
@@ -1,6 +1,16 @@
+import os
+
 class TelaPrincipal:
 
+    def limparTela(self):
+        if os.name == 'nt': 
+            os.system('cls')
+        else:
+            os.system('clear')
+
     def opcoes_do_sistema(self):
+        self.limparTela()
+        print('-=-=-=-=-=-=-TELA INICIAL-=-=-=-=-=-=-')
         print('1 - Notas       2 - Cadastros      3 - Caixas       4 - Sair')
         op = int(input())
         
