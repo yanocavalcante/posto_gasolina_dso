170175787a2aec7aa4e40b3e33d6d437b87afd3e
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-10-16 10:14:03 -0300

    Estilização no terminal

--- views/tela_caixa.py
+++ views/tela_caixa.py
@@ -1,18 +1,25 @@
-class TelaCaixa:
+from views.tela_principal import TelaPrincipal
+
+
+class TelaCaixa(TelaPrincipal):
+
     def seleciona_caixa(self):
-        print('CAIXAS')
+        self.limparTela()
+        self.cabecalho('Caixas')
         caixa = input('Caixa:')
         return caixa
     
     def seleciona_acao(self):
-        print('CAIXAS')
+        self.limparTela()
+        self.cabecalho('Caixas')
         print('1 - Cadastrar    2 - Consultar       3 - Voltar')
         op = int(input())
         return op
     
     def input_info_caixa(self):
-        print('CAIXAS')
-        print('Tipos')
+        self.limparTela()
+        self.cabecalho('Caixas')
+        self.subcabecalho('Tipos')
         print('1 - Físico       2 - Bancário')
         tipo = input()
         nome = input('Nome:')
