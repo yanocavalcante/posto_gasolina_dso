cef9513027f75d0679e924e9e664813cef6a34b9
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-10-16 10:43:52 -0300

    Reestruturação da aba 'Cadastro'

--- controllers/ctrlcadastros.py
+++ controllers/ctrlcadastros.py
@@ -1,17 +1,19 @@
-from views.tela_cadastro import TelaCadastros
+from views.tela_cadastro import TelaCadastro
 from views.tela_pessoa import TelaPessoa
 
 
 class CtrlCadastros:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, ctrlprincipal) -> None:
+        self.__ctrlprincipal = ctrlprincipal
+        self.__tela_cadastro = TelaCadastro()
         
-    
-    def input_opcao_cadastros(self):
-        opcao = TelaCadastros().mostra_opcoes()
+
+    def pergunta_categoria(self):
+        op = self.__tela_cadastro.mostra_opcoes()
+        if op == 1:
+            self.cadastra_produto()
+        elif op == 2:
+            self.cadastra_cliente()
+        elif op == 3:
+            self.cadastra_funcionario()
         
-        if opcao == 2:
-            opcao_pessoa = TelaPessoa().mostra_opcoes_pessoa()
-            if opcao_pessoa == 1:
-                info_pessoa = TelaPessoa().input_cadastro_pessoa()
-                breakpoint()
