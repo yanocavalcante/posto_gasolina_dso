9c554b08a06512f2e6fad886684c21baf7247c45
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-11-18 15:23:04 -0300

    Melhorias em Tela Nota Saída

--- views/tela_nota_saida.py
+++ views/tela_nota_saida.py
@@ -4,18 +4,19 @@ import PySimpleGUI as sg
 class TelaNotaSaida:
     def __init__(self):
         self.__window = None
-        self.init_components()
+        self.input_cliente()
 
-    def init_components(self):
+    def input_cliente(self):
         sg.theme('DarkAmber')
-        layout = [
-                  [sg.Text('NOTA DE SAÍDA')],
-                  [sg.Text('Vai romar no cu iago')]
+        layout= [
+                 [sg.Text('Cliente:')],
+                 [sg.B('Confirmar')]
         ]
-    
+        self.__window = sg.Window('Sistema').Layout(layout)
+
     def open(self):
         event, values = self.__window.Read()
-        if event == sg.WINDOW_CLOSED or event == 'Voltar':
-            return event
-        else:
-            return values
+        return values
+    
+    def close(self):
+        self.__window.Close()
