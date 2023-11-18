95dc2b34d7d2897cce861adf1ecb301af1a1f0fc
yanocavalcante <yano.melo.cavalcante@gmail.com>
2023-11-18 15:23:35 -0300

    Criação da Tela Nota Entrada

---
+++ views/tela_nota_entrada.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as sg
+
+
+class TelaNotaEntrada:
+    def __init__(self):
+        self.__window = None
+        self.input_fornecedor()
+
+    def input_fornecedor(self):
+        sg.theme('DarkAmber')
+        layout = [
+                  [sg.Text('Fornecedor:')],
+                  [sg.B('Confimar')]
+        ]
+        self.__window = sg.Window('Sistema').Layout(layout)
+
+    def open(self):
+        event, values = self.__window.Read()
+        return values
+    
+    def close(self):
+        self.__window.Close()
